c4c4ea1d4fa594f85e59dc8f7fb828c8fd2ccb24
johnson <johnson@basics.fun>
2024-04-22 21:30:07 +0530

    added multi match condition exception

--- api/user_identification.py
+++ api/user_identification.py
@@ -1,9 +1,7 @@
 import json
 from api import dbcon
 
-
-def identify_user_from_database(email: str, phone_number: str) -> dict:
-
+def get_query_based_on_condition(email: str, phone_number: str):
     if phone_number is not None and email is not None:
         query = f"""SELECT * FROM public.contact
             where email = %(email)s or "phoneNumber" = (select "phoneNumber" from public.contact where email = %(email)s limit 1)  or "phoneNumber"=%(phoneNumber)s or email = (select email from public.contact where "phoneNumber"=%(phoneNumber)s limit 1 )
@@ -18,36 +16,80 @@ def identify_user_from_database(email: str, phone_number: str) -> dict:
         query = f"""SELECT * FROM public.contact
             where email = %(email)s or "phoneNumber" = (select "phoneNumber" from public.contact where email = %(email)s limit 1)
             ORDER BY contact."linkPrecedence" ASC """
+    else:
+        raise Exception("Either Email or Phone Number must be passed")
+    
+    return query 
+
+def identify_user_from_database(email: str, phone_number: str) -> dict:
+
+    query = get_query_based_on_condition(email=email,phone_number=phone_number)
+    
 
     df = dbcon.processquery(
         query=query, args={"email": email, "phoneNumber": phone_number}
     )
     
-    
-    df_json = json.loads(df.to_json(orient='records'))
-    
-
+    if df.empty:
+        insert_user(phoneNumber=phone_number,email=email,linkPrecedence='primary')
+        
+        df = dbcon.processquery(
+        query=query, args={"email": email, "phoneNumber": phone_number}
+        )
+        
     primary_row = df.loc[df['linkPrecedence'] == 'primary']['id']
     
+    print(primary_row)
+    
     if len(primary_row) == 1:
+        
+        
         primaryContatctId = primary_row[0]
 
         phone_num_list_list = df['phoneNumber'].to_list()
         
+        phone_num_list_list = filter_list(phone_num_list_list)
+        
         email_list = df['email'].to_list()
+        
+        email_list = filter_list(email_list)
             
-    print(primary_row)
+
+        data_to_send_back = {
+            "contact":{
+                "primaryContatctId": primaryContatctId,
+                "emails": email_list,
+                "phoneNumbers": phone_num_list_list,
+                "secondaryContactIds": [23]
+            }
+        }
+
+        return data_to_send_back
+    elif len(primary_row) > 1:
+        print(df)
+        raise Exception ("Multiple match conditions found")
+
+def insert_user(phoneNumber, email, linkPrecedence):
+    insert_data_query = """INSERT INTO public.contact(
+	 "phoneNumber", email, "linkedId", "linkPrecedence")
+	VALUES (%(phoneNumber)s, %(email)s, (select id from public.contact where email = %(email)s or "phoneNumber" = %(phoneNumber)s), %(linkPrecedence)s) returning id;"""
     
-    data_to_send_back = {
-		"contact":{
-			"primaryContatctId": primaryContatctId,
-			"emails": email_list,
-			"phoneNumbers": phone_num_list_list,
-			"secondaryContactIds": [23]
-		}
-	}
+    vars = {
+        "phoneNumber":phoneNumber,
+        "email":email,
+        "linkPrecedence":linkPrecedence
+    }
+    
 
-    return data_to_send_back
+    new_id = dbcon.excute_query(query=insert_data_query,args=vars)
+    
+    return new_id
 
-def insert_user(phoneNumber, email, linkedId, linkPrecedence):
-    pass
+def filter_list(unflitered_list):
+    filtered_list = [x for x in unflitered_list if x is not None]
+
+    temp_list = set()
+    # Filter out repeated elements
+    filtered_list = [x for x in unflitered_list if x is not None and not (x in temp_list or temp_list.add(x))]
+    
+    return filtered_list
--- api/views.py
+++ api/views.py
@@ -8,12 +8,23 @@ from api.user_identification import identify_user_from_database
 @api_view(['GET','POST'])
 def index_view(request):
 
-    email = request.data.get('email',None)
-    
-    phone_number = request.data.get('phoneNumber',None)
+    try:
+        email = request.data.get('email',None)
+        
+        phone_number = request.data.get('phoneNumber',None)
 
-    data_to_send_back = identify_user_from_database(email=email,phone_number=phone_number)
-    
-    response = Response(data_to_send_back)
+        data_to_send_back = identify_user_from_database(email=email,phone_number=phone_number)
+        
+        response = Response(data_to_send_back)
 
-    return response
+        return response
+    except Exception as e:
+        
+        data_to_send_back = {
+            "message":"error",
+            "error":str(e)
+        }
+
+        response = Response(data_to_send_back,status=500)
+
+        return response
